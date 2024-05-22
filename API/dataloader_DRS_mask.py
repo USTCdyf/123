60582339e435da576492ab7f1914af0ccddb09db
Morris <109666488+junyuanM@users.noreply.github.com>
2024-05-22 19:38:16 +0800

    Update dataloader_DRS_mask.py

--- API/dataloader_DRS_mask.py
+++ API/dataloader_DRS_mask.py
@@ -21,14 +21,9 @@ class VilDataset(Dataset):
             a=np.load(f'{root}/{file}')
             print('npy_data_shape:', a.shape)
             cut = a.shape[0]
-            # data.append(a[:(cut*rate)//100])
-            # data.append(a[(cut*rate)//100:(cut*rate)//50])
             data.append(a[(cut - (cut*rate)//100):])
 
         self.data = np.concatenate(data)
-        #print(self.data.shape)
-        #N, L, H, W = self.data.shape
-        # self.data = self.data.reshape([N  L, H, W])
         self.transform = transform
         self.mean = 0
         self.std = 1
@@ -40,12 +35,8 @@ class VilDataset(Dataset):
         img = self.data[index].reshape(20, 3, 128, 128)
         if self.transform:
             img = self.transform(img)
-        # if np.random.rand() > 0.5:
-        #     img = img[:, :, ::-1].copy()
         input_img = img[:10]
         output_img = img[10:20]
-        # input_img = img[:10]
-        # output_img = img[10:]
         input_img = torch.from_numpy(input_img)
         output_img = torch.from_numpy(output_img)
         input_img = input_img.contiguous().float()
@@ -74,19 +65,3 @@ if __name__ == '__main__':
     dataset = VilDataset(root='../data')
 
     input_img, output_img = dataset[1]
-    # Assuming `input_img` is a NumPy array of shape (10, 64, 64, 1)
-    # fig, axes = plt.subplots(nrows=1, ncols=10)
-
-    # for i in range(10):
-    #     axes[i].imshow(input_img[i, :, :, 0], cmap=None)
-    #     axes[i].axis('off')
-
-    # plt.show()
-
-    # fig, axes = plt.subplots(nrows=1, ncols=10)
-
-    # for i in range(10):
-    #     axes[i].imshow(output_img[i, :, :, 0], cmap=None)
-    #     axes[i].axis('off')
-
-    # plt.show()
