9554edad0d128cf3ab76cdd96d66d3934c7c0d45
Morris <109666488+junyuanM@users.noreply.github.com>
2024-05-22 20:37:14 +0800

    Update Exp_mau.py

--- Exp/Exp_mau.py
+++ Exp/Exp_mau.py
@@ -7,7 +7,6 @@ import pickle
 import logging
 import numpy as np
 import sys
-sys.path.append('/data01/dyf/CaPaint')
 from models.mau_model import MAU_Model
 from omegaconf import OmegaConf
 from tqdm import tqdm
