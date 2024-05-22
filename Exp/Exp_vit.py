42c5b9f29916d27b0c3aaffef1f9213db3b12031
Morris <109666488+junyuanM@users.noreply.github.com>
2024-05-22 19:49:34 +0800

    Update Exp_vit.py

--- Exp/Exp_vit.py
+++ Exp/Exp_vit.py
@@ -54,9 +54,7 @@ class Exp:
         return device
 
     def _preparation(self):
-        # seed
         set_seed(self.args.seed)
-        # log and checkpoint
         self.path = osp.join(self.args.res_dir, self.args.ex_name)
         check_dir(self.path)
 
@@ -71,15 +69,11 @@ class Exp:
             logging.root.removeHandler(handler)
         logging.basicConfig(level=logging.INFO, filename=osp.join(self.path, 'log.log'),
                             filemode='a', format='%(asctime)s - %(message)s')
-        # prepare data
         self._get_data()
-        # build the model
         self._build_model()
 
     def _build_model(self):
         args = self.args
-        # self.model = Nvwa_enchane_SimVP(tuple(args.in_shape), args.hid_S,
-        #                    args.hid_T, args.N_S, args.N_T).to(self.device)
         self.model = VisionTransformer(
             img_size=128,
             patch_size=16,
@@ -90,8 +84,6 @@ class Exp:
             num_heads=1,
             mlp_ratio=4.0
         ).to(self.device)
-        
-
 
     def _get_data(self):
         config = self.args.__dict__
@@ -99,7 +91,6 @@ class Exp:
         self.vali_loader = self.test_loader if self.vali_loader is None else self.vali_loader
 
     def _select_optimizer(self):
-        #Adam
         self.optimizer = torch.optim.Adam(
             self.model.parameters(), lr=self.args.lr)
         self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
@@ -126,13 +117,9 @@ class Exp:
             train_pbar = tqdm(self.train_loader)
 
             for batch_x, batch_y in train_pbar:
-                # print("batch_x:", batch_x.size())
-                # print("batch_y:", batch_y.size())
                 self.optimizer.zero_grad()
                 batch_x, batch_y = batch_x.to(self.device), batch_y.to(self.device)
                 pred_y= self.model(batch_x)
-                #print("pred_size:", pred_y.size())
-                #print("batch_size:", batch_y.size())
                 loss = self.criterion(pred_y, batch_y)
                 train_loss.append(loss.item())
                 train_pbar.set_description('train loss: {:.4f}'.format(loss.item()))
@@ -161,9 +148,6 @@ class Exp:
         preds_lst, trues_lst, total_loss = [], [], []
         vali_pbar = tqdm(vali_loader)
         for i, (batch_x, batch_y) in enumerate(vali_pbar):
-            # if i * batch_x.shape[0] > 1000:
-            #     break
-
             batch_x, batch_y = batch_x.to(self.device), batch_y.to(self.device)
             pred_y = self.model(batch_x)
             list(map(lambda data, lst: lst.append(data.detach().cpu().numpy()), [
@@ -177,11 +161,7 @@ class Exp:
         total_loss = np.average(total_loss)
         preds = np.concatenate(preds_lst, axis=0)
         trues = np.concatenate(trues_lst, axis=0)
-        # mse, mae= metric(preds, trues, vali_loader.dataset.mean, vali_loader.dataset.std, True)
-        # print_log('vali mse:{:.4f}, mae:{:.4f}'.format(mse, mae))
         self.model.train()
-
-
         return total_loss
 
     def test(self, args):
@@ -200,9 +180,5 @@ class Exp:
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        # mse, mae = metric(preds, trues, self.test_loader.dataset.mean, self.test_loader.dataset.std, True)
-        # print_log('mse:{:.4f}, mae:{:.4f}'.format(mse, mae))
-
         for np_data in ['inputs', 'trues', 'preds']:
             np.save(osp.join(folder_path, np_data + '.npy'), vars()[np_data])
-        #return mse
