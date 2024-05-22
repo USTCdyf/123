2da78415e888d0bd0fab5514baf79a8f6c0a4d35
Morris <109666488+junyuanM@users.noreply.github.com>
2024-05-22 19:54:29 +0800

    Update make_KTH_mask1npy.py

--- make_inpaint_data/make_KTH_mask1npy.py
+++ make_inpaint_data/make_KTH_mask1npy.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 
 folder_path = './KTH_inpaint_mask1/'
-# folder_names = [f for f in sorted(os.listdir(folder_path))]
 
 def extract_number(folder_name):
     return int(''.join(filter(str.isdigit, folder_name)))
